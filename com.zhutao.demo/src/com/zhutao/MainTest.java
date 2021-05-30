6503b7ad0fda26f5f7cb85fdbcec15d0eb16a829
taotao-one <215894814@qq.com>
2021-05-30 23:59:43 +0800

    第三次修改 测试回退

--- com.zhutao.demo/src/com/zhutao/MainTest.java
+++ com.zhutao.demo/src/com/zhutao/MainTest.java
@@ -3,5 +3,6 @@ package com.zhutao;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("test cange");
     }
 }
